247ad878b0b8598edad19cd32120f5ee1de0a178
kvelardi <kristenvelardi@gmail.com>
2023-07-19 01:12:59 -0400

    finished up exercises

--- app.js
+++ app.js
@@ -50,34 +50,40 @@ const removeRandom = items => {
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {
-
-}
+const extend = (array1, array2) => {
+    return [...array1, ...array2];
+};
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-// function addKeyVal(obj, key, val) {
-
-// }
+const addKeyVal = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = val;
+    return newObj;
+};
 
 
 /** Return a new object with a key removed. */
 
-// function removeKey(obj, key) {
-
-// }
+const removeKey= (obj, key) => {
+    let newObj = {...obj}
+    delete newObj[key]
+    return newObj;
+};
 
 
 /** Combine two objects and return a new object. */
 
-// function combine(obj1, obj2) {
-
-// }
+const combine = (obj1, obj2) => {
+    return {...obj1, ...obj2};
+};
 
 
 /** Return a new object with a modified key and value. */
 
-// function update(obj, key, val) {
-
-// }
+const update = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = value;
+    return newObj;
+}
